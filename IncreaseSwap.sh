6551db5447f2fb0d470ab64203b92f25268f4598
Sadegh-Kalami <kalami1378@yahoo.com>
2024-02-24 13:59:41 +0330

    Swap + CPULoad

---
+++ IncreaseSwap.sh
@@ -0,0 +1,8 @@
+free -h
+# 30G /swapfile size of sawpfile
+echo '<SYSTEM-Password>' | sudo -S fallocate -l 30G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+free -h
